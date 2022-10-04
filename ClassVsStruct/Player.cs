5a30d9aa4258f202f05669acea453b07368d7dc6
andre-pedro <andrewgoesgaming@gmail.com>
2022-10-04 10:22:52 +0100

    2nd exercise

---
+++ ClassVsStruct/Player.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassVsStruct.obj
+{
+    public class Player
+    {
+        public int Health {get; set;} 
+        public int Armor {get; set;}
+
+    
+    }
+}
---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassVsStruct
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Player p1 = new Player() { Health = 100, Armor = 100 };
+            Player p2 = p1;
+
+
+            Console.WriteLine("HP do P1: " + p1.Health + "Armor do P1: " + p1.Armor);
+            Console.WriteLine("HP do P2: " + p2.Health + "Armor do P2: " + p2.Armor);
+
+            p1.Armor = 11;
+            p2.Armor = 66;
+
+            Console.WriteLine("HP do P1: " + p1.Health + "Armor do P1: " + p1.Armor);
+            Console.WriteLine("HP do P2: " + p2.Health + "Armor do P2: " + p2.Armor)
+        }
+    }
+}
